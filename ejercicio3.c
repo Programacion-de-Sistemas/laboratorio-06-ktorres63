ea9009c88eba068f0d695d4f3789ab79d44b230c
karlo <ktorres@unsa.edu.pe>
2022-06-18 00:47:51 -0500

    ejercicio3 resuelto

---
+++ ejercicio3.c
@@ -0,0 +1,11 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+    char** two = join(reverse(whiteSquare),whiteSquare);
+    char** line = repeatH(two,4);
+    char** twolines= up(line,flipV(line));
+    char** sq = repeatV(twolines,2);
+
+    interpreter(sq);
+}
