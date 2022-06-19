14bfdc01ee6fcf83a233121584fb0da3bd3a16fc
karlo <ktorres@unsa.edu.pe>
2022-06-19 16:28:37 -0500

    avance con la lista enlazada

--- linkedList/linked_list.c
+++ linkedList/linked_list.c
@@ -1,54 +1,57 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-//definimos nuestra node
-typedef struct node
-{
+typedef struct node{
     int number;
-    struct node *next;
-}
-node;
+    struct node* next;
+}node;
 
 int main(){
-    node *list=NULL;
+    int tam,cont;
+    node* list = NULL; //HEAD
+    node* head = NULL;
+    node* curr = head;
+    head = malloc(sizeof(node));
 
-    //agregamos un elemento a la lista
-    node *n = malloc(sizeof(node)); //asignando memoria para node
-    if(n==NULL){
-        return 1;
+    printf("ingrese tamaño de la lista: \n");
+    scanf("%d",&tam);
+
+    while(curr!=NULL){
+        scanf("%d",curr->number);
+        curr = curr->next;
+
     }
+
+/*
+    node* n = malloc(sizeof(node));
+    if(n==NULL)
+        return 1;
     n->number=45;
     n->next=NULL;
 
-    node *n_2 = malloc(sizeof(node)); //asignando memoria para node
-    if(n_2==NULL){
+    node* n_2 = malloc(sizeof(node));
+    if(n_2==NULL)
         return 1;
-    }
-    n_2->number=15;
-    n_2->next=NULL;
 
-    //actualizar el punto next del primer elemento
+    n_2->number = 15;
+    n_2->next =NULL;
+
     n->next=n_2;
+*/
 
 
-    //actualizamos la lista
-    list=n;
+    //list=n;
 
-    // imprimir los elementos de la lista
-    for (node *tmp = list;tmp!=NULL;tmp=tmp->next)
-    {
+    //IMPRIMIR
+    for(node* tmp = head;tmp!=NULL;tmp=tmp->next)
         printf("%i\n",tmp->number);
 
-
+    //FUNCION FREE BUENA PRACTICA EN PROGRAMACION
+    while(head!=NULL){
+        node* tmp = head->next;
+        free(head);
+        head=tmp;
     }
-
-    // función (free) BUENA PR´ACTICA EN PROGRAMACI´ON EN "C"
-    while(list!=NULL){
-        node *tmp=list->next;
-        free(list);
-        list=tmp;
-    }
-
     return 0;
-
 }
+
