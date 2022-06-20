397875c9224e7f992d3a49d5ce6a0d7b9adc6582
karlo <ktorres@unsa.edu.pe>
2022-06-19 19:12:55 -0500

    completo el ej de linked list

--- linkedList/linked_list.c
+++ linkedList/linked_list.c
@@ -8,44 +8,38 @@ typedef struct node{
 
 int main(){
     node* list = NULL; //HEAD
-    
+
     node* head = NULL;
     node* temp = NULL;
-    
+
     node* curr = head;
 
-    int tam,cont;
+    int tam;
     printf("ingrese tamaño de la lista: \n");
     scanf("%d",&tam);
 
     for(int i=0;i<tam;i++){
         temp = malloc(sizeof(node));
-        scanf("%d",&temp->number);
+        //scanf("%d",&temp->number);
+        temp->number=i;
         temp->next=NULL;
+
+        if(head==NULL){
+            head=temp;
+            curr=head;
+        }
+        else{
+            curr->next=temp;
+            curr=temp;
+        }
     }
-/*
-    node* n = malloc(sizeof(node));
-    if(n==NULL)
-        return 1;
-    n->number=45;
-    n->next=NULL;
-
-    node* n_2 = malloc(sizeof(node));
-    if(n_2==NULL)
-        return 1;
-
-    n_2->number = 15;
-    n_2->next =NULL;
-
-    n->next=n_2;
-*/
-
+    curr= head;
 
     //list=n;
 
     //IMPRIMIR
     for(node* tmp = head;tmp!=NULL;tmp=tmp->next)
-        printf("%i\n",tmp->number);
+        printf("imprimiendo: %i\n",tmp->number);
 
     //FUNCION FREE BUENA PRACTICA EN PROGRAMACION
     while(head!=NULL){
@@ -55,4 +49,3 @@ int main(){
     }
     return 0;
 }
-
